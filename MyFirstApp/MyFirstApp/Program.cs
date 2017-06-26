ef16d4e0d61c39f85b4d89d11da012341b17907b
Do Quang Tung <dqt@globeteam.com>
2017-06-26 14:32:21 +0700

    NEw Feature

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
+            var commitsFile = File.ReadAllLines(@"C:\CommitsOfMaster.txt");
+
+            var commits = commitsFile.Select(t => {
+                return t.Split(' ')[0];
+            }).ToList();
+
+            commits.Reverse();
+
+            commits = commits.Select(t => "git cherry-pick --strategy=recursive -X theirs " + t).ToList();
+            var cherryPick = string.Join(Environment.NewLine, commits);
         }
     }
 }
